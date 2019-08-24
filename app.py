c1c610b6a9b2fb7996e82ed968fc8e6109fc4664
fbpotato <54449780+fbpotato@users.noreply.github.com>
2019-08-24 14:12:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return 'hello'
+    return render_template('home.html')
+
+@app.route('/about/')
+def about():
+    return 'about'
+
 if __name__ == '__main__':
     app.run()
